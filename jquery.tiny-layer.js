d8870f577f4dca4c3d327ba7a9aceb2206649ab5
Florian Capelle <floriancapelle@users.noreply.github.com>
2016-06-21 16:59:37 +0200

    Major development
    still things to be done

--- jquery.tiny-layer.js
+++ jquery.tiny-layer.js
@@ -5,141 +5,197 @@
 (function( $ ) {
     'use strict';
 
+    // Public API
     var api = {};
 
     /* Configuration
      * closeBtnMarkup {string} - markup of the close button to be appended, false if not
      * autoCloseOthers {bool} - close other open layers when opening a layer
-     * cssClasses {string} - supply css classes if you want, e.g. to identify a layer in css
      * events {object} - supply jQuery-like events with delegation, delegateTarget is item root
      */
     var defaults = {
         appendTo: 'body',
         triggerSelector: '[data-layer-target]',
         triggerTargetKey: 'layerTarget',
+        layerItemClass: 'tiny-layer-item',
+        layerItemContentClass: 'layer-item-content',
+        layerItemTpl: '<article class="tiny-layer-item"><div class="layer-item-content"></div></article>',
         layerOptions: {
-            closeBtnMarkup: '<button class="tiny-layer-item-close btn btn-default btn-has-icon" type="button"><svg class="svg-icon svg-icon-cross"><use xlink:href="#still-cross"></use></svg></button>',
+            closeBtnMarkup: '<button class="layer-item-close" type="button">x</button>',
             autoCloseOthers: true,
             closeOnOverlayClick: true,
-            cssClasses: '',
-            events: {}
+            events: []
         }
     };
+    var conf = {};
     var $root;
     var $layerItemTpl;
-    var uniqIdBase = 0;
-    var layers = {};
-
-    /*$.fn.tinyLayer = function( options ) {
-        return this;
-    };*/
 
     /*
-     * Anforderungen
-     * - tiny, nur die notwendigsten funktionen
-     * - layer inhalt kann alles sein
-     * - mehrere layer müssen gleichzeitig möglich sein
-     * - kein init oder add, layer werden bei klick des triggers erst initialisiert
-     * - events müssen pro layer komfortabel möglich sein
-     *   - events schon am layer registrieren und clone(true)?
-     * - Jeder Layer ist inklusive overlay komplett mit css anpassbar
-     *
-     * TODO
-     *
-     * - add entfernen und bei open einsetzen, ggf. mehrere functions nutzen
+     * @TODO
+     * - close icon mit css statt svg icon
+     * - animationen bei open und close
      */
 
     /**
      * Initialize the component
+     * @param {{}} customConf - custom configuration to overwrite the defaults
+     * @returns {{}}
      */
-    function init() {
+    function init( customConf ) {
+        conf = $.extend(true, {}, defaults, customConf);
+
+        // create wrapper and append to configured element
         $root = $('<aside class="tiny-layer"></aside>');
-        $layerItemTpl = $('<article class="tiny-layer-item"><div class="tiny-layer-item-content"></div></article>');
+        $layerItemTpl = $(conf.layerItemTpl);
 
-        $(defaults.appendTo).append($root);
+        $(conf.appendTo).append($root);
 
-        $('body').on('click.tinyLayer', defaults.triggerSelector, function( event ) {
+        // trigger event handling
+        // open target layer on click on a trigger
+        $('body').on('click.tinyLayer', conf.triggerSelector, function( event ) {
             var $trigger = $(this);
+            var layerId = $trigger.data(conf.triggerTargetKey);
 
-            open($trigger.data([defaults.triggerTargetKey]));
+            // stop if the layer already has been added
+            if ( getLayerInRoot(layerId).length ) return;
+
+            initLayer(layerId);
+            open(layerId);
         });
 
-        $root.on('click.tinyLayer', '.tiny-layer-item', function( event ) {
+        // overlay event handling
+        // close layer on click on overlay
+        $root.on('click.tinyLayer', '.' + conf.layerItemClass, function( event ) {
             var $layer = $(this);
 
             if ( !$layer.is(event.target) ) return;
-            if ( $layer.data('options').layerOptions.closeOnOverlayClick !== true ) return;
-            close($layer);
+            if ( $layer.data('options').closeOnOverlayClick !== true ) return;
+            close($layer.data('id'));
         });
-        $root.on('click.tinyLayer', '.tiny-layer-item-close', function( event ) {
-            close($(this).closest('.tiny-layer-item'));
+        // close layer on click on close btn
+        $root.on('click.tinyLayer', '.layer-item-close', function( event ) {
+            close($(this).closest('.' + conf.layerItemClass).data('id'));
         });
+
+        // Reveal public api
+        $.extend(api, {
+            open: open,
+            close: close,
+            // replace with no-op to just call this once
+            init: function() {return api;}
+        });
+        $.tinyLayer = api;
+
+        return api;
     }
 
-    function add( newLayerContent, options ) {
-        if ( typeof newLayerContent === 'undefined' ) return;
+    /**
+     * Initialize a layer by copying the source layer content and appending it to a new layer
+     * @param layerId - layer id like '#layer', will be used as jQuery selector
+     * @returns {boolean} - whether the initialization was successful or not
+     */
+    function initLayer( layerId ) {
+        if ( !layerId ) return false;
 
-        options = $.extend(true, {}, defaults.layerOptions, options);
-        uniqIdBase += 1;
+        var $sourceLayer = $(layerId);
+        if ( !$sourceLayer || !$sourceLayer.length ) return false;
 
+        var layerContent = $sourceLayer.html();
+        if ( typeof layerContent === 'undefined' ) return false;
+
+        // create a new layer item
         var $newLayer = $layerItemTpl.clone();
-        var $newLayerContent = $newLayer.children('.tiny-layer-item-content');
+        var $newLayerContent = $newLayer.children('.' + conf.layerItemContentClass);
+        // merge options with defaults and options on the source layer tag if defined
+        // like: data-close-btn-markup="false" => closeBtnMarkup: false
+        var options = $.extend(true, {}, conf.layerOptions, $sourceLayer.data());
 
-        $newLayerContent.append(newLayerContent);
+        // set id and options for later use
+        $newLayer.data('id', layerId);
+        $newLayer.data('options', options);
+        // append the source markup to the new item content
+        $newLayerContent.append(layerContent);
 
         if ( options.closeBtnMarkup ) {
             $newLayerContent.append($(options.closeBtnMarkup));
         }
-        $newLayer.addClass(options.cssClasses);
-        $newLayer.on(options.events);
-        $newLayer.data('id', uniqIdBase);
-
-        layers[uniqIdBase] = {
-            $el: $newLayer,
-            options: options
-        };
+        // copy all classes from target layer to new layer item
+        // except the hide class
+        $newLayer.addClass($sourceLayer.attr('class').replace('tiny-layer-hide', ''));
+        // apply custom events if defined
+        options.events.forEach(function( val ) {
+            $newLayer.on(val);
+        });
 
         $root.append($newLayer);
-
-        return uniqIdBase;
+        return true;
     }
 
-    function open( layerSelector ) {
-        if ( !layerSelector ) return api;
+    /**
+     * open a layer
+     * @param layerId
+     * @returns {{}}
+     */
+    function open( layerId ) {
+        layerId = layerId || '';
 
-        var $layer = $root.children(layerSelector);
-        if ( !$layer || !$layer.length ) return api;
-
-        if ( !$layer.data('options') ) {
-            $layer.data('options', $.extend(true, {}, defaults.layerOptions));
+        var $layer = getLayerInRoot(layerId);
+        // if the layer is not initialized yet, do it and open it afterwards
+        if ( !$layer || !$layer.length ) {
+            if ( initLayer(layerId) === true ) {
+                open(layerId);
+            }
+            return api;
         }
+
         var layerOptions = $layer.data('options');
 
         if ( layerOptions.autoCloseOthers === true ) {
-            close($root.children('.is-visible'))
+            // close every child layer that's visible
+            $root.children('.is-visible').each(function() {
+                close($(this).data('id'));
+            });
         }
         $layer.addClass('is-visible');
 
         return api;
     }
 
-    function close( layer ) {
-        layer = layer || '';
+    /**
+     * Close and remove a layer
+     * @param layerId
+     * @returns {{}}
+     */
+    function close( layerId ) {
+        if ( !layerId ) return api;
 
-        $root.children(layer).removeClass('is-visible');
+        //$root.children('[data-id="' + layerId + '"]').removeClass('is-visible');
+        getLayerInRoot(layerId).remove();
 
         return api;
     }
 
-    // Public API
-    $.extend(api, {
-        open: open,
-        close: close
-    });
+    /**
+     * Get layer element by id in wrapper, match layerId with data property
+     * @param layerId - must match the
+     * @returns {*|HTMLElement}
+     */
+    function getLayerInRoot( layerId ) {
+        var $layer = $();
 
-    $(function() {
-        init();
-        $.tinyLayer = api;
-    });
+        $root.children().each(function() {
+            if ( $(this).data('id') === layerId ) {
+                $layer = $(this);
+                return false;
+            }
+        });
+
+        return $layer;
+    }
+
+    $.tinyLayer = {
+        init: init
+    };
 
 }(jQuery));
